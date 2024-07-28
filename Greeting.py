42cfd012138103726a0b09a01892710019e56c79
Vinayak Khandelwal <139864778+VinayakKhandelwal07@users.noreply.github.com>
2024-07-28 21:23:02 +0530

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,19 @@
+
+
+
+
+import time 
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime("%H"))
+name = input("Enter Your Name :")
+
+# print(hour)
+if hour >= 0 and hour < 12:
+    print("Good Morning!",name)
+elif hour>=12 and hour <17:
+    print("Good Afernoon!",name)
+elif hour>= 17 and hour > 0:
+    print("Good Night!",name)
+
+
+
